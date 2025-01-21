1ea2f9ade8e8f55a4fbe0a87b063252838d057f5
Sandesh Shinde <shindesandesh2398@gmail.com>
2025-01-22 01:12:37 +0530

    backend project commit

---
+++ SpringJobAppWithUI/src/main/java/com/example/SpringJobApp/SpringJobAppWithUiApplication.java
@@ -0,0 +1,15 @@
+package com.example.SpringJobApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJobAppWithUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJobAppWithUiApplication.class, args);
+
+		System.out.println("CHECK");
+	}
+
+}
---
+++ SpringJobAppWithUI/src/test/java/com/example/SpringJobApp/SpringJobAppWithUiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringJobApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJobAppWithUiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
